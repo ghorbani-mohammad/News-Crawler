95f7e07a1a1361db0c16da14c44fca0478e4dd61
Mohammad <aali361@gmail.com>
2022-01-16 18:40:15 +0330

    feat: rm comments

--- app/settings.py
+++ app/settings.py
@@ -3,21 +3,10 @@ import environ
 
 env = environ.Env()
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'n6ld+$-+#x(j7!vys)uvbscvsmm51nwn+(z#3zeqjx+a-!vt_@'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = [env.str('SERVER_IP')]
 SERVER_IP = env.str('SERVER_IP')
 DB_PORT = env.str('DB_PORT')
 DB_USER = env.str('DB_USER')
@@ -82,10 +71,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'app.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
 # Password validation
