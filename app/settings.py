0f4796109a2186c6c3c76f8399619cda26b41bf3
Mohammad <aali361@gmail.com>
2022-01-20 13:32:22 +0330

    feat: minor change

--- app/settings.py
+++ app/settings.py
@@ -18,7 +18,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': DB_USER,
         'PASSWORD': DB_PASS,
-        'HOST': 'rasad_postgres',
+        'HOST': 'postgres',
         'PORT': DB_PORT,
     },
 }
