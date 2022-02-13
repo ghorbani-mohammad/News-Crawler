289248ffb0b23d1efa5b423f99e62dd412fc21c4
Mohammad <aali361@gmail.com>
2022-02-13 23:42:49 +0330

    fix: minor change

--- crawler/settings.py
+++ crawler/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "app.urls"
+ROOT_URLCONF = "crawler.urls"
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "app.wsgi.application"
+WSGI_APPLICATION = "crawler.wsgi.application"
 
 
 # Password validation
