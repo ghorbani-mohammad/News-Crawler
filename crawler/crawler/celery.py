960bcb56ff37728409937b5c084617bf694c3253
Mohammad <aali361@gmail.com>
2022-02-18 15:10:46 +0330

    feat: minor change

--- crawler/crawler/celery.py
+++ crawler/crawler/celery.py
@@ -7,24 +7,23 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crawler.settings")
 
-# TODO: redis port and ip and db must be dynamic
 crawler = Celery("crawler")
-
-# Optional configuration, see the application user guide.
 crawler.config_from_object("django.conf:settings")
 crawler.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 # if you want to purge works queue
 crawler.control.purge()
 
+MINUTE = 60
+
 crawler.conf.beat_schedule = {
     "check-agencies-60-seconds": {
         "task": "check_agencies",
-        "schedule": 10 * 60,
+        "schedule": 10 * MINUTE,
     },
     "redis-exporter-300-seconds": {
         "task": "redis_exporter",
-        "schedule": 5 * 60,
+        "schedule": 5 * MINUTE,
     },
     "remove_obsolete_reports": {
         "task": "remove_obsolete_reports",
