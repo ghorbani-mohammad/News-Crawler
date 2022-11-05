f09efc4d12b00e32fcb6375a33ae11292cffb146
Mohammad <mohammad.ghorbani@hotmail.com>
2022-11-05 21:35:04 +0330

    minor change

--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -70,9 +70,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "crawler.wsgi.application"
 
 
-# Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
