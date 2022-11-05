261e3f5c85c359b70078a02b238946b6efa94c26
Mohammad <mohammad.ghorbani@hotmail.com>
2022-11-05 21:21:45 +0330

    minor change

--- crawler/agency/admin.py
+++ crawler/agency/admin.py
@@ -1,6 +1,6 @@
 from prettyjson import PrettyJSONWidget
-from django.contrib import admin
 from django import forms
+from django.contrib import admin
 from django.utils.html import format_html
 from rangefilter.filter import DateTimeRangeFilter
 
