26b211a738df619a4d277809754eeb3fe384937a
Mohammad <mohammad.ghorbani@hotmail.com>
2022-11-05 21:33:56 +0330

    minor change

--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -92,9 +92,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
+USE_TZ = False
 USE_I18N = True
 USE_L10N = True
-USE_TZ = False
 TIME_ZONE = "UTC"
 LANGUAGE_CODE = "en-us"
 
