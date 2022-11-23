74eea0cbd4c276b97fcae394def2070b8fd074de
Mohammad <mohammad.ghorbani@hotmail.com>
2022-11-23 22:19:15 +0330

    minor change

--- crawler/agency/urls.py
+++ crawler/agency/urls.py
@@ -15,11 +15,11 @@ urlpatterns = [
     path("fetch_content/", views.FetchContent.as_view()),
     path("agency/<int:agency_id>/pages/", views.agency_pages),
     path("crawl/agency/active_all/", views.crawl_agency_activeAll),
-    path("crawl/agency/disable_all/", views.crawl_agency_disableAll),
     path("crawl/agency/<int:agency_id>/", views.crawl_agency),
     path("crawl/memory/reset/", views.crawl_memory_reset),
     path("crawl/news/memory/list/", views.crawl_news_memory_list),
     path("crawl/links/memory/list/", views.crawl_links_memory_list),
+    path("crawl/agency/disable_all/", views.crawl_agency_disableAll),
     path("crawl/memory/reset/<int:agency_id>/", views.crawl_agency_reset_memory),
     path(
         "crawl/agency/reset_and_crawl/<int:agency_id>/",
