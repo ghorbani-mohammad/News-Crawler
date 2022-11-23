981f8eec18ff9867fbaab57173bb004934d3b40d
Mohammad <mohammad.ghorbani@hotmail.com>
2022-11-23 22:40:07 +0330

    minor change

--- crawler/agency/models.py
+++ crawler/agency/models.py
@@ -4,7 +4,7 @@ from django.contrib.postgres.fields import JSONField
 
 class Agency(models.Model):
     name = models.CharField(max_length=20, null=False, unique=True)
-    country = models.CharField(max_length=20, default='NA')
+    country = models.CharField(max_length=20, default="NA")
     website = models.CharField(max_length=100, null=False, unique=True)
     alexa_global_rank = models.IntegerField(default=0, null=True)
     crawl_headers = JSONField(null=True, blank=True, default=dict)
@@ -14,14 +14,14 @@ class Agency(models.Model):
     deleted_at = models.DateTimeField(null=True, blank=True)
 
     class Meta:
-        ordering = ['id']
+        ordering = ["id"]
 
     def __str__(self):
-        return self.name
+        return f"({self.pk} - {self.name})"
 
 
 class AgencyPageStructure(models.Model):
-    agency = models.ForeignKey(Agency, related_name='pages', on_delete=models.CASCADE)
+    agency = models.ForeignKey(Agency, related_name="pages", on_delete=models.CASCADE)
     url = models.CharField(max_length=300, null=False, unique=True)
     crawl_interval = models.IntegerField(default=12)
     last_crawl = models.DateTimeField(null=True, editable=False)
@@ -38,7 +38,9 @@ class AgencyPageStructure(models.Model):
 
 
 class CrawlReport(models.Model):
-    page = models.ForeignKey(AgencyPageStructure, on_delete=models.CASCADE, related_name='report')
+    page = models.ForeignKey(
+        AgencyPageStructure, on_delete=models.CASCADE, related_name="report"
+    )
     status = models.CharField(max_length=300, null=True)
     fetched_links = models.IntegerField(default=0)
     new_links = models.IntegerField(default=0)
