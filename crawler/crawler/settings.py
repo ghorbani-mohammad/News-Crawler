3ac2475c7981238c9eb18dfa9e374af223f6dfd6
Mohammad <mohammad.ghorbani@hotmail.com>
2022-11-24 22:36:28 +0330

    minor change

--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -103,10 +103,10 @@ REST_FRAMEWORK = {
 }
 
 # Celery
-CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
 CELERY_TIMEZONE = "Asia/Tehran"
+CELERY_ACCEPT_CONTENT = ["application/json"]
 BROKER_URL = "redis://news_crawler_redis:6379/10"
 CELERY_RESULT_BACKEND = "redis://news_crawler_redis:6379/10"
 
