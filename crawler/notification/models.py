ae759944faeec22d65aa4bc6881f125bd7a81dee
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-12 22:59:07 +0330

    minor change

--- crawler/notification/models.py
+++ crawler/notification/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class BaseModelAbstract(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
